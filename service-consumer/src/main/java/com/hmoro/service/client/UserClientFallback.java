c16bded37c0a968691b5351aa1429738975af590
henuhpf <han024589>
2019-08-15 12:52:03 +0800

    配置zuul

--- service-consumer/src/main/java/com/hmoro/service/client/UserClientFallback.java
+++ service-consumer/src/main/java/com/hmoro/service/client/UserClientFallback.java
@@ -8,14 +8,14 @@ public class UserClientFallback implements UserClient {
     @Override
     public User queryUserById(Long id) {
         User user = new User();
-        user.setUserName("服务器忙，请稍后再访问");
+        user.setUsername("服务器忙，请稍后再访问");
         return user;
     }
 
     @Override
     public User hystrixQueryUserById(Long id) {
         User user = new User();
-        user.setUserName("服务器忙，请稍后再访问");
+        user.setUsername("服务器忙，请稍后再访问");
         return user;
     }
 }
